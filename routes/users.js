b6a78e4714a4323d0b368176a9782519bb50a570
xiaqijian <2817062995@qq.com>
2018-04-27 11:07:26 +0800

    add mysql

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
+const mysql = require('mysql');
 var router = express.Router();
+
+const connection = mysql.createConnection({
+  host: '101.132.140.59',
+  user: 'minapp',
+  password: 'minapp123xia',
+  port: '3306',
+  database: 'minapp',
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/taobao', (req, res, next) => {
+  connection.connect();
+
+  var sql = 'SELECT * FROM taobao';
+  //查
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log('[SELECT ERROR] - ', err.message);
+      return;
+    }
+    res.send(result);
+
+    console.log('--------------------------SELECT----------------------------');
+    console.log(result);
+    console.log('------------------------------------------------------------\n\n');
+  });
+
+  connection.end();
+})
 
 module.exports = router;
