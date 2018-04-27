5effb98d84c4f78a90fcbe1c7e56e3f9c042725c
xiaqijian <2817062995@qq.com>
2018-04-27 13:41:57 +0800

    利用mysql的连接池处理限制mysql限制

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,17 @@ var express = require('express');
 const mysql = require('mysql');
 var router = express.Router();
 
-const connection = mysql.createConnection({
+const dbconfig = {
   host: '101.132.140.59',
   user: 'minapp',
   password: 'minapp123xia',
   port: '3306',
-  database: 'minapp',
-});
-connection.connect();
+  database: 'minapp'
+}
+
+// var connection = mysql.createConnection(dbconfig);
+var pool = mysql.createPool(dbconfig)
+// connection.connect();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -21,16 +24,29 @@ router.get('/taobao', (req, res, next) => {
 
   var sql = 'SELECT * FROM taobao';
   //查
-  connection.query(sql, function (err, result) {
-    if (err) {
-      console.log('[SELECT ERROR] - ', err.message);
-      return;
-    }
-    res.send(result);
+  // connection.query(sql, function (err, result) {
+  //   if (err) {
+  //     console.log('[SELECT ERROR] - ', err.message);
+  //     return;
+  //   }
+  //   res.send(result);
 
-    console.log('--------------------------SELECT----------------------------');
-    console.log(result);
-    console.log('------------------------------------------------------------\n\n');
+  //   console.log('--------------------------SELECT----------------------------');
+  //   console.log(result);
+  //   console.log('------------------------------------------------------------\n\n');
+  // });
+  pool.getConnection(function (err, conn) {
+    if (err) console.log("POOL ==> " + err);
+
+    conn.query(sql, function (err, rows) {
+      if (err) console.log(err);
+      console.log("SELECT ==> ");
+      res.send(rows);
+      // for (var i in rows) {
+      //     console.log(rows[i]);
+      // }
+      conn.release();
+    });
   });
 
   // connection.end();
